7961ebbd0fcd97604be60b917b5563f85ef21d87
w00tl00ps <w00tl00ps@gmail.com>
2023-11-05 23:47:57 -0500

    chore: cleanup code

--- src/routes/api/organsites/+server.js
+++ src/routes/api/organsites/+server.js
@@ -14,19 +14,6 @@ export async function GET() {
     const data = await biomarkers.distinct("OrganSite");
 
     console.log(data);
-    // create a cleaned copy of the search results
-    /*const cleanedData = data.map( d => ({
-        _id: d._id.toString(),
-        ShortName: d.ShortName,
-        OrganSite: d.OrganSite,
-        Biomarkers: d.Biomarkers,
-        Indication: d['Testing Indication'],
-        FundedDate: d['CCO Funded Date'],
-        TestingSite: d['Testing Sites'],
-        Notes: d['Notes']
-    }))*/
 
-    // return the Array of search results
-    // DEBUG: console.log(cleanedData);
     return json(data);   
 }
