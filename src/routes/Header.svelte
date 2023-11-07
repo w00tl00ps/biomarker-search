a429828b5dd19b9eb98630d630483a8c0917aa85
w00tl00ps <w00tl00ps@gmail.com>
2023-11-06 22:06:22 -0500

    fix: remove default Sverdle component

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -15,9 +15,6 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
 		  </ul>
 		</div>
 	</div>
