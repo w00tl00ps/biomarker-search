7ca86ad83c131302b3338a208fb8279c9d463a22
w00tl00ps <w00tl00ps@gmail.com>
2023-11-07 22:18:30 -0500

    Update "about" page content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,4 +10,19 @@
 		This is a web app that serves to catalogue the list of <a href="https://www.cancercareontario.ca/en/cancer-care-ontario/programs/clinical-services/pathology-laboratory-medicine">Funded Molecular Testing in Ontario</a> and display
 		the ever-growing-list in an easy to search format.
 	</p>
+	<h2>Built With</h2>
+	<div class="not-prose">
+		<img src="https://img.shields.io/badge/svelte-%23f1413d.svg?style=for-the-badge&logo=svelte&logoColor=white" alt="SvelteKit">
+		<img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="TailwindCSS">
+		<img src="https://img.shields.io/badge/daisyui-5A0EF8?style=for-the-badge&logo=daisyui&logoColor=white" alt="DaisyUI">
+		<img src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white" alt="MongoDB">
+		<img src="https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white" alt="Vercel">
+	</div>
+		
 </div>
+
+<style>
+	img {
+		margin-top: 0.5rem;
+	}
+</style>
