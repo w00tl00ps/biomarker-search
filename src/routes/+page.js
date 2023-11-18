71e4d5a9589bc6fafba4269e35a4377c4c1bc6d7
w00tl00ps <w00tl00ps@gmail.com>
2023-11-17 23:43:44 -0500

    refactor: initial biomarker list from page load is obtained from API instead of a redundant query

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,20 +1,19 @@
 
 
 /** @type {import('./$types').PageLoad} */
-export const load = async function( {parent, data, fetch} ) {
-
-    // get the data passed from +.page.server.js
-    await parent();
-    let { biomarkers } = data
-    //console.log("+page.js biomarker data: ", biomarkers);
+export const load = async function( {fetch} ) {
 
     // get organ site list from /api/organsites
     const response = await fetch(`/api/organsites`);
     const result = await response.json();
     console.log("/routes/+page.js: GET from /api/organsites");
 
+    // get all biomarkers from /api/biomarkers
+    const allBiomarkers = (await fetch('/api/biomarkers?search=')).json();
+    console.log("/routes/+page.js: GET from /api/biomarkers?search=");
+
     return {
-        biomarkers,
+        biomarkers: allBiomarkers,
         organSites: result
     }
 }
