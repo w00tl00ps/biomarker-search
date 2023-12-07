e55e6ec039ecd8b0db69795ddd0f9c5abf505a9a
w00tl00ps <w00tl00ps@gmail.com>
2023-12-06 23:21:15 -0500

    update layout for small screens

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,16 +1,33 @@
 <script>
 	import { page } from '$app/stores';
+	import 'iconify-icon';
 </script>
 
 <header>
 	<div class="navbar bg-primary text-primary-content">
 		<div class="navbar-start">
+			<div class="dropdown">
+				<div tabindex="0" role="button" class="btn btn-ghost md:hidden">
+				  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+				</div>
+				<ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+					<li>
+						<a href="/">Search</a>
+					</li>
+					<li>
+						<a href="/updates">Recent Updates</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+				</ul>
+			</div>
 		  <a class="btn btn-ghost normal-case text-xl" href="/">CCO Biomarker Search</a>
 		</div>
-		<div class="navbar-end">
+		<div class="navbar-end hidden md:flex">
 		  <ul class="menu menu-horizontal px-1">
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Search</a>
+				<a href="/"><iconify-icon icon="ic:baseline-search" width="25" height="25"></iconify-icon></a>
 			</li>
 			<li aria-current={$page.url.pathname === '/updates' ? 'page' : undefined}>
 				<a href="/updates">Recent Updates</a>
