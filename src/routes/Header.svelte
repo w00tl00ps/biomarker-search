97af01572ad9e9716ae36b2889b14a5a05db462f
w00tl00ps <w00tl00ps@gmail.com>
2023-12-06 23:29:39 -0500

    change menu color to improve readability

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -10,7 +10,7 @@
 				<div tabindex="0" role="button" class="btn btn-ghost md:hidden">
 				  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 				</div>
-				<ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+				<ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-primary rounded-box w-52">
 					<li>
 						<a href="/">Search</a>
 					</li>
